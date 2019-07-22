545fdf002325694a48d87eb5e392c3e0b6122532
Edan Sneh <edansneh@Edans-MacBook-Pro.local>
2019-07-21 23:50:58 -0700

    wave moves

--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Wave from './Components/Wave.js'
+import Slider from '@material-ui/core/Slider';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {incr: 0}
+    this.state = {func: this.tick(), incr: 0};
+    this.tick = this.tick.bind(this);
+    this.interval = setInterval(this.tick, 10);
+  }
+
+  tick() {
+    this.setState((state) => {
+      return {incr: state.incr + Math.PI/40}
+    });
+    const func = (array) => { 
+      let data = [];
+      array.forEach((x) => data.push(Math.cos(x+this.state.incr)));
+      return data;
+    }
+    return func;
+  }
+  render() {
+    console.log(this.state.incr);
+    return (
+      <div className="App">
+        <header className="App-header">
+          <div><h1>Wave</h1></div>
+          <div className="Wave"><Wave func={this.state.func}/></div>
+          <div className="Sliders">
+            Sliders
+            <br/>
+            <div className="Slider-heads">Amplitude</div>
+          <Slider
+            defaultValue={20}
+            // getAriaVlueText={"valuetext"}
+            aria-labelledby="discrete-slider-custom"
+            step={1}
+            valueLabelDisplay="auto"
+            // marks={marks}
+          />
+          <Slider
+            defaultValue={20}
+            // getAriaVlueText={"valuetext"}
+            aria-labelledby="discrete-slider-custom"
+            step={1}
+            valueLabelDisplay="auto"
+            // marks={marks}
+          />
+          </div>
+        </header>
+      </div>
+      
+    );
+  }
 }
 
 export default App;
---
+++ src/Components/Wave.js
@@ -0,0 +1,52 @@
+import React from 'react';
+
+class Wave extends React.Component {
+    
+    constructor(props) {
+        super();
+        this.state = {color: '#2196F3', strokeSize: 60};
+    } 
+
+    makePath() {
+        const datax = range(0, 30, .1);
+
+        
+
+        function range(start, end, interval) {
+            var ans = [];
+            for (let i = start; i <= end; i += interval) {
+                ans.push(i);
+            }
+            return ans;
+        }
+        // const func = (array) => {
+        //     let data = [];
+        //     array.forEach((x) => data.push(Math.cos(x)));
+        //     return data;
+        // }
+        let datay = this.props.func(datax);
+        let svgData = "M " + datax[0]*this.state.strokeSize + " " + datay[0]*this.state.strokeSize;
+        for (let i = 1; i < datax.length; i++) {
+            svgData += " L " + datax[i]*this.state.strokeSize + " " + datay[i]*this.state.strokeSize;    
+        }
+        return (<path d={svgData} style={{strokeWidth: 10, stroke: this.state.color, fillOpacity: 0}}/>);
+    }
+
+    makeAxis() {
+        return (<path d="M 0 0 L 10000 0" style={{stroke: 'gray', fillOpacity: 0}}/>)
+    };
+
+    render() {
+        return (
+            <svg
+                width="800"
+                height="390"
+                viewBox="500 -500 1000 1000">
+                {this.makePath()};
+                {this.makeAxis()};
+            </svg>
+        );
+    }
+}
+
+export default Wave;
